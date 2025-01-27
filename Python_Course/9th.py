511928d3d947080f62c3935f8a613fd256845def
Tomas Matukas <tomas.matukas@gmail.com>
2025-01-27 22:04:26 +0200

    added exercise 5

--- Python_Course/9th.py
+++ Python_Course/9th.py
@@ -1,5 +1,6 @@
 
 # - - - - - - - U Z D U O T I S  - -- 1 - -- - - - - - - - - - -
+print('--------------------Užduotis 1-------------------------------------')
 
 # Įvadas į funkcijas
 # Užduotis 1:
@@ -10,7 +11,7 @@ def pasveikink():
         print('Sveikinu atvykus ! ! !')
 
 pasveikink()
-print('---------------------------------------------------------')
+print('-------------------Užduotis 2--------------------------------------')
 
 # - - - - - - - U Z D U O T I S  - -- 2 - -- - - - - - - - - - -
 # Argumentai ir return reikšmės
@@ -30,7 +31,7 @@ if suma:
 else:
     print('Numbers is not provided!')
 
-print('---------------------------------------------------------')
+print('----------------Užduotis 3-----------------------------------------')
 
 # - - - - - - - U Z D U O T I S  - -- 3 - -- - - - - - - - - - -
 
@@ -41,11 +42,11 @@ print('---------------------------------------------------------')
 # Pvz.: „Labas, Jonas!“, „Labas, Asta!“, „Labas, Mantas!“.
 
 def pasisveikinimas_su_naujais_vartotojais(vardas1, vardas2, vardas3):
-    sveiki = f'Labas  {vardas1}! Labas {vardas2}! Ir Labas {vardas3}'
+    sveiki = f'Labas  {vardas1}!, Labas {vardas2}!, Ir Labas {vardas3}'
     return sveiki
 res = pasisveikinimas_su_naujais_vartotojais('Jonas', 'Asta', 'Mantas!')
 print(res)
-print('---------------------------------------------------------')
+print('----------------Užduotis 4-----------------------------------------')
 # 4. Numatytosios reikšmės ir keyword argumentai
 # Užduotis 4:
 # Sukurkite funkciją sveikink_su_pavadinimu(vardas, pavadinimas="drauge"), kuri
@@ -58,6 +59,34 @@ def sveikink_su_pavadinimu(vardas, pavadinimas='drauge'):
 sveikink_su_pavadinimu('Tomas')
 sveikink_su_pavadinimu('Petras', pavadinimas='kainyne')
 
-print('---------------------------------------------------------')
+print('-------------------Užduotis 5--------------------------------------')
+
+# - - - - - - - T A S K  - -- 4 - -- - - - - - - - - - -
+
+# Loginiai jungikliai funkcijose
+# Užduotis 5:
+# Sukurkite funkciją skaiciuoti_sumos_tipą(x, y, tik_teigiama=False), kuri
+# priimtų du skaičius ir grąžintų jų sumą.
+# • Jei tik_teigiama=True, funkcija grąžintų tik teigiamą sumą (jei suma neigiama,
+# grąžintų 0).
+
+def skaiciuoti_sumos_tipą(x, y, tik_teigiama=False):
+    res = x + y
+    if not tik_teigiama:
+        return res
+    if res < 0:
+        return 0
+    return res
+print(skaiciuoti_sumos_tipą(24, -45, True))
+
+# 2 ------------------- variantas-------------------------------------
+def skaiciuoti_sumos_tipą(x: int, y: int, tik_teigiama=False) -> int:
+    res = x + y
+    if tik_teigiama:
+        return max(suma, 0)
+    return suma
+print(skaiciuoti_sumos_tipą(5, 3))
+print(skaiciuoti_sumos_tipą(5, -30, True))
+print('-------------------Užduotis 6--------------------------------------')
 
 
