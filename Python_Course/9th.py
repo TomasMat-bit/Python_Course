1905772cd87608d719605f9d7e499c80b4484ec9
Tomas Matukas <tomas.matukas@gmail.com>
2025-01-28 18:25:45 +0200

    update added exercice 6 and 7

--- Python_Course/9th.py
+++ Python_Course/9th.py
@@ -89,4 +89,46 @@ print(skaiciuoti_sumos_tipą(5, 3))
 print(skaiciuoti_sumos_tipą(5, -30, True))
 print('-------------------Užduotis 6--------------------------------------')
 
+# Docstringai funkcijose
+# Užduotis 6:
+# Sukurkite funkciją apskaiciuok_vidurki(skaiciai), kuri apskaičiuotų ir grąžintų
+# sąrašo skaičių vidurkį. Pridėkite docstring su informacija apie:
+# • Funkcijos paskirtį.
+# • Argumentus (sąrašas skaičių).
+# • Grąžinamą reikšmę (vidurkis).
 
+def apskaiciuok_vidurki(skaiciai):
+    """
+    Apskaičiuoja ir grąžina sąrašo skaičių vidurkį.
+
+    Argumentai:
+    skaiciai (list): Sąrašas skaičių.
+
+    Grąžinama reikšmė:
+    float: Skaičių vidurkis.
+    """
+    if not skaiciai:
+        return 0  # Jei sąrašas tuščias, grąžiname 0
+    return sum(skaiciai) / len(skaiciai)
+
+print(apskaiciuok_vidurki([1, 2, 3, 4, 5]))
+
+print('-------------------Užduotis 7--------------------------------------')
+# Type hints ir anotacijos
+# Užduotis 7:
+# Sukurkite funkciją prideti_zodi(tekstas: str, zodis: str) -> str, kuri priimtų
+# sakinį ir pridedamą žodį, o tada grąžintų sakinį su tuo žodžiu gale.
+def prideti_zodi(tekstas: str, zodis: str) -> str:
+    """
+    Prideda žodį prie sakinio galo.
+
+    Argumentai:
+    tekstas (str): Pradinis sakinys.
+    zodis (str): Žodis, kurį reikia pridėti.
+
+    Grąžinama reikšmė:
+    str: Atnaujintas sakinys su pridėtu žodžiu gale.
+    """
+    return tekstas + " " + zodis
+
+print(prideti_zodi("Labas, pasauli!", "kaip sekasi?"))
