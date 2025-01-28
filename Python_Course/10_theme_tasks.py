b2b5543e5368933441760fb689ec476ec6e23e53
Tomas Matukas <tomas.matukas@gmail.com>
2025-01-28 19:39:29 +0200

    added ex 5 and 6

--- Python_Course/10_theme_tasks.py
+++ Python_Course/10_theme_tasks.py
@@ -46,3 +46,28 @@ print(f"Dabartinė data ir laikas: {dt.datetime.now().strftime('%Y-%m-%d %H:%M:%
 
 
 print(' - - - - - Uzduotis 5 - - - - - - - -')
+
+# 5. Specifinių funkcijų importavimas su alias
+# Užduotis 5:
+# Sukurkite Python programą, kuri:
+# 1. Importuoja sqrt funkciją iš math modulio su alias sq.
+# 2. Apskaičiuoja kvadratinę šaknį iš 625 naudojant sq().
+# 3. Išspausdina rezultatą.
+
+from math import sqrt as sq
+print(sq(625)) #atsakymas - 25.0
+
+print(' - - - - - Uzduotis 6 - - - - - - - -')
+
+# 6. Visko importavimas iš modulio (nerekomenduojamas būdas)
+# Užduotis 6:
+# 1. Sukurkite Python programą, kuri naudoja from math import *.
+# 2. Apskaičiuoja sinusą iš 90 laipsnių (naudojant sin()).
+# 3. Išspausdina rezultatą.
+# 4. Parašykite komentaruose, kodėl šis metodas gali būti pavojingas dideliuose
+# projektuose.
+
+from math import *
+print(sin(90)) #atsakymas - 0.8939966636005579
+
+
