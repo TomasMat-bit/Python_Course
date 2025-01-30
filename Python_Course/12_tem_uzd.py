13ec8da287aa78986bd86741aa1db9f72bfaedaf
Tomas Matukas <tomas.matukas@gmail.com>
2025-01-30 20:39:52 +0200

    added ex 4

--- Python_Course/12_tem_uzd.py
+++ Python_Course/12_tem_uzd.py
@@ -22,29 +22,62 @@ print(dalinti(10,2))
 print(dalinti(5,0))
 print(dalinti(8,4))
 
-print('- - - - - - - - - Užduotis 3: - - - - - - - - - - - - ')
+# print('- - - - - - - - - Užduotis 3: - - - - - - - - - - - - ')
+#
+# # Užduotis 3:
+# # 1. Paprašykite vartotojo įvesti du skaičius.
+# # 2. Naudokite try-except, kad suvaldytumėte:
+# # a. ValueError, jei vartotojas įveda ne skaičių.
+# # b. ZeroDivisionError, jei dalinama iš nulio.
+# # 3. Jei klaidos nėra, išveskite rezultatą
+#
+# input1 = input('irasykite skaiciu: ')
+# input2 = input('irasykite skaiciu: ')
+#
+# try:
+#     a = int(input1)
+#     b = int(input2)
+#     res = a / b
+#     print(f'Your result is: {res}')
+# except ZeroDivisionError:
+#     print('Change 0 to another integer')
+# except ValueError:
+#     print('Use numbers')
+# print('----------------- Destytojo kodas -----------------------------')
+# while True:
+#     ivestis1 = input('Iveskite sveika skaiciu: ')
+#     ivestis2 = input('Iveskite dalikli: ')
+#
+#     try:
+#         skaicius = int(ivestis1)
+#         daliklis = int(ivestis2)
+#         res = skaicius / daliklis
+#         print(f'Rezultatas: {res}')
+#     except ZeroDivisionError:
+#         print('Pakeiskite dalikli is 0 i kita.')
+#     except ValueError:
+#         print('Paleiskite programa is naujo ir isvestis padarykit, kad tai butu sveikas skaicius.')
 
-# Užduotis 3:
-# 1. Paprašykite vartotojo įvesti du skaičius.
-# 2. Naudokite try-except, kad suvaldytumėte:
-# a. ValueError, jei vartotojas įveda ne skaičių.
-# b. ZeroDivisionError, jei dalinama iš nulio.
-# 3. Jei klaidos nėra, išveskite rezultatą
-
-input1 = input('irasykite skaiciu: ')
-input2 = input('irasykite skaiciu: ')
-
-try:
-    a = int(input1)
-    b = int(input2)
-    res = a / b
-    print(f'Your result is: {res}')
-except ZeroDivisionError:
-    print('Change 0 to another integer')
-except ValueError:
-    print('Use numbers')
-print('----------------------------------------------')
 
 print('- - - - - - - - - Užduotis 4: - - - - - - - - - - - - ')
+
+# Sukurkite programą, kuri:
+# 1. Prašo vartotojo įvesti skaičių.
+# 2. Bandys konvertuoti į int.
+# 3. Jei klaidos nėra, naudos else, kad atspausdintų "Konversija sėkminga:
+# <skaičius>".
+# 4. finally bloke atspausdins "Programa baigė darbą.", nepriklausomai nuo
+# klaidų.
+
+try:
+    user_input = input('Įveskite skaičių: ')
+    number = int(user_input)
+except ValueError:
+    print('Klaida: įvestas ne skaičius.')
+else:
+    print(f'Konversija sėkminga: {number}')
+finally:
+    print("Programa baigė darbą.")
+
 
 
