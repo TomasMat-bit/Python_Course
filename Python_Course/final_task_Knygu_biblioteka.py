339775c58503c4e941e96fa2308cf12977d86897
Tomas Matukas <tomas.matukas@gmail.com>
2025-02-05 19:44:02 +0200

    update step 2

--- Python_Course/final_task_Knygu_biblioteka.py
+++ Python_Course/final_task_Knygu_biblioteka.py
@@ -21,7 +21,7 @@ print('- - - - - - - - - --  ŽINGSNIS 1: Sukurkite klasę Book- - - - - - - - -
 # o __str__ – grąžina informaciją apie knygą gražiu formatu.
 # o is_classic() – grąžina True, jei knyga išleista daugiau nei prieš 50 metų.
 
-import datetime
+
 
 class Book:
     def __init__(self, title, author, year, available=True):
@@ -37,6 +37,7 @@ class Book:
     def is_classic(self):
         current_year = datetime.datetime.now().year
         return current_year - self.year > 50
+print('- - - - - - - - -ŽINGSNIS 2: Sukurkite klasę Library- - - - - - - - - - - - - - ')
 
 # ŽINGSNIS 2: Sukurkite klasę Library
 # • Laukai:
@@ -52,16 +53,60 @@ class Book:
 # ▪ Pvz.: filter_books(author="Jonas") arba
 # filter_books(year=2000)
 
+class Library:
+    def __init__(self):
+        self.books = []
 
+    def add_book(self, book):
+        self.books.append(book)
 
+    def display_books(self):
+        if not self.books:
+            print('Biblioteka tuscia.')
+        for book in self.books:
+            print(book)
 
+    def borrow_book(self, title):
+        try:
+            book = next(book for book in self.books if book.title == title)
+            if book.available:
+                book.available = False
+                print(f'Jūs pasiskolinote knygą: {book.title}')
+            else:
+                print('Knyga jau pasiskolinta.')
+        except StopIteration:
+            print(f'Knyga su pavadinimu "{title}" nerasta.')
 
+    def return_book(self, title):
+        try:
+            book = next(book for book in self.books if book.title == title)
+            book.available = True
+            print(f'Knyga "{book.title}" buvo grąžinta.')
+        except StopIteration:
+            print(f'Knyga su pavadinimu "{title}" nerasta.')
 
+    def filter_books(self, *args, **kwargs):
+        filtered_books = self.books
+        if args:
+            pass
+        for key, value in kwargs.items():
+            filtered_books = [book for book in filtered_books if getattr(book, key, None) == value]
+
+        if not filtered_books:
+            print('Nerasta atitinkančių knygų.')
+        for book in filtered_books:
+            print(book)
+
+print('- - - - - - - - -ŽINGSNIS 3: Naudokite datetime paskolos valdymui- - - - - - - - - - - - - - ')
 
 # ŽINGSNIS 3: Naudokite datetime paskolos valdymui
 # • Kai vartotojas pasiskolina knygą, įrašykite paskolos datą.
 # • Pridėkite metodą due_date(), kuris grąžins datą, kada knyga turi būti grąžinta
 # (pvz., po 14 dienų).
+
+import datetime
+
+
 # ŽINGSNIS 4: Sukurkite sąveiką su vartotoju
 # • Naudokite while ciklą, kad vartotojas galėtų:
 # o Pridėti naują knygą į biblioteką.
