879af8210fad6cc6916da401c17a8073af15ba7b
Tomas Matukas <tomas.matukas@gmail.com>
2025-02-10 21:12:09 +0200

    created tasks 3

--- Python_Course/17th_theme_tasks.py
+++ Python_Course/17th_theme_tasks.py
@@ -186,3 +186,62 @@ except AttributeError as e:
 
 
 print('-' * 30)
+
+print(' - - - - - - - - - - - -  UZDUOTIS 3  - - - - - - - - - - - ')
+
+# 3. @property Dekoratorius
+# Užduotis:
+# Sukurkite klasę Darbuotojas, kuri turi atributus vardas, pavarde ir privatų atributą
+# __atlyginimas.+
+# • Naudokite @property dekoratorių, kad galėtumėte gauti ir nustatyti atlyginimą.
+# • Užtikrinkite, kad atlyginimas negali būti mažesnis nei minimalus (pvz., 500).
+# Papildoma užduotis:
+# Pridėkite papildomą @property metodą mokesciai, kuris apskaičiuoja mokesčius (pvz.,
+# 20% nuo atlyginimo).
+
+class Darbuotojas:
+    def __init__(self, vardas, pavarde, asmens_kodas, atlyginimas = 500):
+        self.vardas = vardas
+        self.pavarde = pavarde
+        self.asmens_kodas = asmens_kodas
+        self.__atlyginimas = atlyginimas
+
+    @property
+    def atlyginimas(self):
+        return self.__atlyginimas
+
+    @atlyginimas.setter
+    def atlyginimas(self, suma):
+        if suma >= 500:
+            self.__atlyginimas = suma
+        else:
+            raise ValueError('Min atlyginimas 500 pinigu')
+
+    @property
+    def mokesciai(self):
+        return self.__atlyginimas * 0.20
+
+    @property
+    def lytis(self):
+        return 'Vyras' if self.asmens_kodas[0] == '3' else 'Moteris'
+
+darbuotojas = Darbuotojas('Jonas', 'Jonavicius', '39694030284')
+
+print(darbuotojas.lytis)
+print(darbuotojas.atlyginimas)
+print(darbuotojas.mokesciai)
+
+print('-' * 30)
+
+darbuotojas.atlyginimas = 2000
+print(darbuotojas.atlyginimas)
+print(darbuotojas.mokesciai)
+
+print('-' * 30)
+
+try:
+    darbuotojas.atlyginimas = 400
+except ValueError as e:
+    print(e)
+
+print(' - - - - - - - - - - - -  UZDUOTIS 4  - - - - - - - - - - - ')
