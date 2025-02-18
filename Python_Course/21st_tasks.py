efa51823b5704832a0493f82a0d0233188ad9482
Tomas Matukas <tomas.matukas@gmail.com>
2025-02-18 18:06:54 +0200

    21st temos papildymas

--- Python_Course/21st_tasks.py
+++ Python_Course/21st_tasks.py
@@ -47,31 +47,10 @@ print(' - - - - 2. Užduotis  - - - - - - - ')
 from sqlalchemy import Column, create_engine, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Sukuriame duomenų bazę
-engine = create_engine('sqlite:///mokykla.db')
-Base = declarative_base()
-
-# Modeliai
-class Mokinys(Base):
-    __tablename__ = 'mokinys'
-    id = Column(Integer, primary_key=True)
-    vardas = Column(String)
-    pavarde = Column(String)
-    klase = Column(Integer)
-
-class Mokytojas(Base):
-    __tablename__ = 'mokytojas'
-    id = Column(Integer, primary_key=True)
-    vardas = Column(String)
-    pavarde = Column(String)
-    dalykas = Column(String)
-
-# Sukuriame lenteles
-Base.metadata.create_all(engine)
-
 # Sukuriame sesiją
 from sqlalchemy import Column, create_engine, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+
 
 # Sukuriame duomenų bazę
 engine = create_engine('sqlite:///mokykla.db')
