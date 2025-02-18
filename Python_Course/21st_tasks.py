92b397f0127d135e50f9174091cbec59cb2bec7c
Tomas Matukas <tomas.matukas@gmail.com>
2025-02-18 21:53:59 +0200

    21st task papildymas dest kodu

--- Python_Course/21st_tasks.py
+++ Python_Course/21st_tasks.py
@@ -327,3 +327,101 @@ def vidutinis_mokiniu_skaicius_klaseje():
 
 vidutinis_mokiniu_skaicius_klaseje()
 print('-' * 30)
+# # print('-'* 30, 'DESTYTOJO KODAS: ','-'* 30)
+#
+# from sqlalchemy import Column, Integer, String, create_engine, func
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.exc import MultipleResultsFound, NoResultFound
+#
+# Base = declarative_base()
+#
+# class Mokinys(Base):
+#     __tablename__ = 'mokiniai'
+#     id = Column(Integer, primary_key=True)
+#     vardas = Column(String)
+#     pavarde = Column(String)
+#     klase = Column(Integer)
+#
+# class Mokytojas(Base):
+#     __tablename__ = 'mokytojai'
+#     id = Column(Integer, primary_key=True)
+#     vardas = Column(String)
+#     pavarde = Column(String)
+#
+# engine = create_engine('sqlite:///school.db')
+# Base.metadata.create_all(engine)
+#
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# def delete_student_by_id(student_id):
+#     student = session.get(Mokinys, student_id)
+#     if student:
+#         session.delete(student)
+#         session.commit()
+#         print(f'Mokinys su ID {student_id} pašalintas.')
+#     else:
+#         print(f'Mokinys su ID {student_id} nerastas.')
+#
+# def delete_teacher_by_id(teacher_id):
+#     teacher = session.get(Mokytojas, teacher_id)
+#     if teacher:
+#         session.delete(teacher)
+#         session.commit()
+#         print(f'Mokytojas su ID {teacher_id} pašalintas.')
+#     else:
+#         print(f'Mokytojas su ID {teacher_id} nerastas.')
+#
+# def delete_graduated_students():
+#     graduated_students = session.query(Mokinys).filter_by(klase=12).all()
+#     for student in graduated_students:
+#         session.delete(student)
+#     session.commit()
+#     print(f'Visi 12 klasės mokiniai pašalinti.')
+#
+# def find_student_by_name(name):
+#     try:
+#         student = session.query(Mokinys).filter_by(vardas=name).one()
+#         print(student)
+#     except NoResultFound:
+#         print(f'Mokinys su vardu {name} nerastas.')
+#     except MultipleResultsFound:
+#         print(f'Rasta daugiau nei viena įrašų su vardu {name}.')
+#
+# def find_students_by_lastname_initial(letter):
+#     students = session.query(Mokinys).filter(Mokinys.pavarde.ilike(f'{letter}%')).all()
+#     for student in students:
+#         print(student)
+#
+# def find_teachers_by_name_endswith(letter):
+#     teachers = session.query(Mokytojas).filter(Mokytojas.vardas.ilike(f'%{letter}')).all()
+#     for teacher in teachers:
+#         print(teacher)
+#
+# def list_students_by_class():
+#     students = session.query(Mokinys).order_by(Mokinys.klase).all()
+#     for student in students:
+#         print(student)
+#
+# def count_students_per_class():
+#     class_counts = session.query(Mokinys.klase, func.count(Mokinys.id)).group_by(Mokinys.klase).all()
+#     for klase, count in class_counts:
+#         print(f'Klasė {klase}: {count} mokiniai')
+#
+# def average_students_per_class():
+#     total_students = session.query(func.count(Mokinys.id)).scalar()
+#     total_classes = session.query(func.count(func.distinct(Mokinys.klase))).scalar()
+#     avg_students = total_students / total_classes if total_classes else 0
+#     print(f'Vidutinis mokinių skaičius klasėje: {avg_students:.2f}')
+#
+#
+# delete_student_by_id(3)
+# delete_teacher_by_id(2)
+# delete_graduated_students()
+# find_student_by_name("Jonas")
+# find_students_by_lastname_initial("P")
+# find_teachers_by_name_endswith("s")
+# list_students_by_class()
+# count_students_per_class()
+# average_students_per_class()
