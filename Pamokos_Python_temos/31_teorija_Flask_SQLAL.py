4e5c82d647dd2ca302b3d8514c46990358a21f5d
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-05 21:49:55 +0200

    teorija of the day 31

---
+++ Pamokos_Python_temos/31_teorija_Flask_SQLAL.py
@@ -0,0 +1,105 @@
+# from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+#
+# app = Flask(__name__)
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projektai.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+#
+# db = SQLAlchemy(app)
+#
+#
+# class Projektas(db.Model):
+#     __tablename__ = "projektas"
+#     id = db.Column(db.Integer, primary_key=True)
+#     pavadinimas = db.Column(db.String)
+#     kaina = db.Column(db.Float)
+#     sukurimo_data = db.Column(db.DateTime, default=datetime.datetime.now)
+#
+#
+#
+#
+#
+# with app.app_context():
+#     db.create_all()
+#
+#
+# @app.route("/")
+# def home():
+#     all_rows = Projektas.query.all()
+#     return render_template("index_31_teorija.html", projektas_rows=all_rows)
+#
+#
+# if __name__ == "__main__":
+#     app.run()
+
+print('-'*50)
+
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projektai.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+
+class Projektas(db.Model):
+    __tablename__ = 'projektas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pavadinimas = db.Column(db.String)
+    kaina = db.Column(db.Float)
+    sukurimo_data = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    @property
+    def kaina_su_pvm(self):
+        return round(self.kaina * 1.21)
+
+    def __init__(self, pavadinimas, kaina):
+        self.pavadinimas = pavadinimas
+        self.kaina = kaina
+
+    def __repr__(self):
+        return f'{self.id} {self.pavadinimas} {self.kaina} {self.sukurimo_data}'
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    search_text = request.args.get('searchlaukelis')
+    if search_text:
+        filtered_rows = Projektas.query.filter(Projektas.pavadinimas.ilike(search_text + '%'))
+        return render_template('index.html', projektas_rows=filtered_rows)
+    all_rows = Projektas.query.all()
+    return render_template('index_31_teorija.html', projektas_rows=all_rows)
+
+@app.route('/prideti-projekta', methods=['GET', 'POST'])
+def new_project():
+    if request.method == 'GET':
+        return render_template('ivedimo_forma.html')
+    elif request.method == 'POST':
+        pavadinimas = request.form.get('laukelispavadinimas')
+        try:
+            kaina = float(request.form.get('laukeliskaina'))
+        except ValueError:
+            raise ValueError('Kaina shoud be float!')
+        if pavadinimas and kaina:
+            new_project_row = Projektas(pavadinimas, kaina)
+            db.session.add(new_project_row)
+            db.session.commit()
+        return redirect(url_for('home')) # home - funkcijos pavinimas(endpoint "/")
+
+
+
+app.run()
