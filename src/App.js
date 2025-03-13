8e87d2e7109b09d0ac76ecd582e5fea618dcecae
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-13 19:46:56 +0200

    TASK 1 and 2 completed

--- src/App.js
+++ src/App.js
@@ -1,84 +1,37 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// function App() {
-//   const name = "TOMAS";
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Tomas is Editing <code>src/App.js</code> and after saving lounching
-//           it.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//TASK 1
-
-// import "./App.css";
-
-// function App() {
-//   const name = "TOMAS";
-//   const age = 25;
-//   const currentYear = new Date().getFullYear();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img
-//           src="https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-//           alt="logo"
-//         />
-//         <h1>
-//           {name} is {age} years old
-//         </h1>{" "}
-//         {}
-//         <p>
-//           Current Year: {currentYear} {}
-//         </p>
-//         <button onClick={() => alert("Sveiki, Tomai!")}>Click Me</button> {}
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//TASK 2
-
 import React from "react";
-import "./App.css";
+import Profilis from "./Profilis";
+import Pasisveikinimas from "./Pasisveikinimas";
 
 function App() {
-  function Profilis({ vardas, pomegis }) {
-    return (
-      <div>
-        <h2>{vardas}</h2>
-        <p>Pomėgis: {pomegis}</p>
-      </div>
-    );
-  }
+  const vardas = "Ona";
+  const pavarde = "Onaite";
+  const amzius = 25;
+  let dabartiniaiMetai = new Date().getFullYear();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Profilis vardas="Jonas" pomegis="Fotografija" />
-        <Profilis vardas="Eva" pomegis="Knygų skaitymas" />
-        <Profilis vardas="Tomas" pomegis="Sportas" />
-      </header>
+    <div>
+      <h1>Sveiki atvyke i React!</h1>
+      <img
+        src="https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="Keliautojos nuotrauka"
+        width="300"
+      />
+      <Pasisveikinimas vardas={vardas} pavarde={pavarde} />
+
+      <p>
+        Vardas: {vardas}, Amzius: {amzius}
+      </p>
+      <p>Dabartiniai metai: {dabartiniaiMetai}</p>
+
+      <button onClick={() => alert("Paspaudei mygruka!")}>Spausti cia</button>
+      <br />
+      <a href="https://react.dev/" target="_blank" rel="noreferrer">
+        React
+      </a>
+
+      <h3>Vartotoju profiliai</h3>
+      <Profilis vardas="Jonas" pomegis="Krepsinis" />
+      <Profilis vardas="Elge" pomegis="Fotografija" />
     </div>
   );
 }
