10e6a5fa02dc9ac143284740b160144587cd1931
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-13 20:20:34 +0200

    35 react teorija

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import React from "react";
-import Profilis from "./Profilis";
-import Pasisveikinimas from "./Pasisveikinimas";
+import Vartotojai from "./Vartotojai";
 
 function App() {
-  const vardas = "Ona";
-  const pavarde = "Onaite";
-  const amzius = 25;
-  let dabartiniaiMetai = new Date().getFullYear();
+  const duomenys = [
+    { id: 1, vardas: "Jonas", amzius: 25 },
+    { id: 2, vardas: "Laura", amzius: 30 },
+    { id: 3, vardas: "Darius", amzius: 20 },
+    { id: 4, vardas: "Ona", amzius: 40 },
+    { id: 5, vardas: "Petras", amzius: 50 },
+  ];
 
   return (
     <div>
-      <h1>Sveiki atvyke i React!</h1>
-      <img
-        src="https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="Keliautojos nuotrauka"
-        width="300"
-      />
-      <Pasisveikinimas vardas={vardas} pavarde={pavarde} />
-
-      <p>
-        Vardas: {vardas}, Amzius: {amzius}
-      </p>
-      <p>Dabartiniai metai: {dabartiniaiMetai}</p>
-
-      <button onClick={() => alert("Paspaudei mygruka!")}>Spausti cia</button>
-      <br />
-      <a href="https://react.dev/" target="_blank" rel="noreferrer">
-        React
-      </a>
-
-      <h3>Vartotoju profiliai</h3>
-      <Profilis vardas="Jonas" pomegis="Krepsinis" />
-      <Profilis vardas="Elge" pomegis="Fotografija" />
+      <Vartotojai vartotojai={duomenys} />
     </div>
   );
 }
