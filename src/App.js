0f22474900890c70ff3b7b627a68386baa01f13a
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-13 20:34:58 +0200

    teorija 35diena, react, SKAICIUOKLE

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React from "react";
-import Vartotojai from "./Vartotojai";
+import React, { useState } from "react";
+import Skaiciuokle from "./Skaiciuokle";
 
 function App() {
-  const duomenys = [
-    { id: 1, vardas: "Jonas", amzius: 25 },
-    { id: 2, vardas: "Laura", amzius: 30 },
-    { id: 3, vardas: "Darius", amzius: 20 },
-    { id: 4, vardas: "Ona", amzius: 40 },
-    { id: 5, vardas: "Petras", amzius: 50 },
-  ];
-
-  return (
-    <div>
-      <Vartotojai vartotojai={duomenys} />
-    </div>
-  );
+  return <Skaiciuokle />;
 }
 
 export default App;
